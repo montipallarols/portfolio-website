b2be2e26d4984c9670abc648753424638b6c3196
montipallarols <montaga.pallarols@gmail.com>
2021-02-25 20:12:27 +0100

    Make home section responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,30 +4,21 @@ import Projects from "../Projects/Projects";
 
 export default function Home() {
   return (
-    <div>
-      <div className="home">
-        <div className="profile">
-          <img
-            // onMouseOver={(e) =>
-            //   (e.currentTarget.src = "/images/mon-cartoon.png")
-            // }
-            // onMouseOut={(e) => (e.currentTarget.src = "/images/mon.png")}
-            src="/images/mon.png"
-            height="350px"
-            alt="profile"
-          />
-          <div className="profile-text">
-            <h2>hey.</h2>
-            <div className="profile-intro">
-              <p>
-                I'm Montaga, lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </div>
+    <div className="home">
+      <div className="profile">
+        <img className="image top" src="/images/mon.png" alt="profile" />
+        <div className="profile-text">
+          <h2>hey.</h2>
+          <div className="profile-intro">
+            <p>
+              I'm Montaga, lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
           </div>
         </div>
+        <img className="image bottom" src="/images/mon.png" alt="profile" />
         {/* <img className="arrow" src="/images/down-arrow.svg" alt="arrow" /> */}
       </div>
       <Projects />
