aefe2e72baed308a991bbfadebb72d99b39477bb
montipallarols <montaga.pallarols@gmail.com>
2021-02-27 11:34:44 +0100

    Make footer responsive on mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,15 +11,19 @@ export default function Home() {
           <h2>hey.</h2>
           <div className="profile-intro">
             <p>
-              I'm Montaga, lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat.
+              I'm Montaga, a junior developer who decided to leave teaching
+              behind to find out how computers use language instead. Soon after
+              I started experimenting with HTML, CSS and JavaScript I realised
+              I'd found the perfect cognitive outlet with the right balance of
+              logic, utility and creativity. It’s been a nonstop sprint ever
+              since and I'm excited to keep growing and work on exciting and
+              challenging projects.
             </p>
           </div>
         </div>
         <img className="image bottom" src="/images/mon.png" alt="profile" />
       </div>
+
       <img className="arrow" src="/images/drawn-arrow.svg" alt="arrow" />
 
       <Projects />
